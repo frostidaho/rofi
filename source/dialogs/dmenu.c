d7c6b479f3cf042d33e20cda0065f6d9672d7668
Idaho Frost <frostidaho@gmail.com>
2017-03-13 21:01:44 -0400

    Rename flag variable

--- source/dialogs/dmenu.c
+++ source/dialogs/dmenu.c
@@ -199,17 +199,17 @@ static GString *get_next_element( DmenuModePrivateData *pd )
     }
     g_string_append(data, firstpart);
     g_free (firstpart);
-    int flag = 0;
+    int cant_read = 0;
     GString *maybesep = g_string_new("");
     for (size_t i=0; i < separator->len; i++) {
       guchar val = g_data_input_stream_read_byte ( pd->data_input_stream, NULL, NULL );
       if (val == 0) {
-        flag = 1;
+        cant_read = 1;
         break;
       }
       g_string_append_c(maybesep, val);
     }
-    if (flag) {
+    if (cant_read) {
       g_string_append(data, maybesep->str);
       g_string_free(maybesep, TRUE);
       break;
